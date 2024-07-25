becbc73e9c0e15f307f3c422edd6c9728b66e7e8
princewritecode <princewritecode@gmail.com>
2024-07-25 12:28:56 +0530

    completed the interactive rating component using html css js done ✅

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const card = document.querySelector('.rating-card');
 const submit = document.querySelector('.button-submit');
 const card1 = document.querySelector('.rating-card1');
+const rating = document.getElementsByClassName('rating-div');
+const ratingSubmited = document.querySelector('.rating-recieved');
+for (let i of rating)
+{
+    i.addEventListener('click', () =>
+    {
+        let span = document.createElement('span');
+        span.innerText = i.innerText;
+        console.log(span);
+        ratingSubmited.appendChild(span);
+    });
+}
 const init = () =>
 {
     card1.classList.add('rating-card-visiblity1');
 };
 submit.addEventListener('click', () =>
 {
-
     for (let i of card.classList)
     {
         if (i.value !== 'rating-card-visiblity')
         {
-
             card.classList.add('rating-card-visiblity');
             card1.classList.remove('rating-card-visiblity1');
-
         }
     };
 });
